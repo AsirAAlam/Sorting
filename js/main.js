eca7deac11546cd813d8483eeebc06c86c11c358
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-02-13 22:25:50 -0500

    added plotting bars

--- js/main.js
+++ js/main.js
@@ -14,4 +14,31 @@ function load() {
     }
     document.getElementById("header__inputNumbers").value = str;
   });
+
+  document.querySelector(".header__plot").addEventListener("click", () => {
+    let arr = document.getElementById("header__inputNumbers").value.split(",");
+    let width = 990.0;
+    let dx = width / arr.length;
+    let sortingBox = document.querySelector(".sortingBox");
+
+    // remove any existing children
+    while (sortingBox.firstChild) sortingBox.removeChild(sortingBox.firstChild);
+
+    for (let i = 0; i < arr.length; i++)
+    {
+      arr[i] = parseInt(arr[i]);
+      let bar = document.createElement("div");
+      bar.className = "bar";
+      bar.style.left = dx * i + 'px';
+      bar.style.height = arr[i] + '%';
+      bar.style.borderLeftWidth = (dx - 2) + 'px';
+      sortingBox.appendChild(bar);
+    }
+
+    console.log(arr);
+  });
+
+  document.querySelector(".header__sort").addEventListener("click", () => {
+    alert("I don't know how to sort yet!");
+  });
 }
