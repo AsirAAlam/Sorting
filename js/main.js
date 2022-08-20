f15e55ff1a0f6a657948eb0dd7855d02af74f3b0
AsirAAlam <Alvi.Abrar29@gmail.com>
2022-08-19 21:26:53 -0400

    bootstrap cdn, some css changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+const gapBetweenBars = 2;
+const sortingBoxWidth = 1000;
+const sortingBoxBorderWidth = 5;
+
 function load() {
+  document.getElementById('app').style.width = sortingBoxWidth.toString() + 'px';
+  document.getElementById('app').style.border = "5px solid black";
+
+  // Generate array of n random numbers
   document.querySelector(".header__generate").addEventListener("click", () => {
     let n = document.getElementById("header__inputLength").value;
     if (n === "") {
@@ -7,7 +15,8 @@ function load() {
     }
     n = parseInt(n);
     let str = "";
-    
+
+    // Generate values: 1-100 inclusive, and append to string
     for (let i = 0; i < n; i++) {
       str += Math.floor(Math.random() * 100) + 1;
       if (i < n - 1) str += ", ";
@@ -15,27 +24,27 @@ function load() {
     document.getElementById("header__inputNumbers").value = str;
   });
 
+  // Plot the the bars using the array values as heights
   document.querySelector(".header__plot").addEventListener("click", () => {
     let arr = document.getElementById("header__inputNumbers").value.split(",");
-    let width = 990.0;
-    let dx = width / arr.length;
+    let canvasWidth = sortingBoxWidth - 2 * sortingBoxBorderWidth + gapBetweenBars;
+    let barWidth = canvasWidth / arr.length;
     let sortingBox = document.querySelector(".sortingBox");
 
-    // remove any existing children
-    while (sortingBox.firstChild) sortingBox.removeChild(sortingBox.firstChild);
+    // Remove any existing children
+    while (sortingBox.firstChild) {
+      sortingBox.removeChild(sortingBox.firstChild);
+    }
 
-    for (let i = 0; i < arr.length; i++)
-    {
+    for (let i = 0; i < arr.length; i++) {
       arr[i] = parseInt(arr[i]);
       let bar = document.createElement("div");
       bar.className = "bar";
-      bar.style.left = dx * i + 'px';
+      bar.style.left = barWidth * i + 'px';
       bar.style.height = arr[i] + '%';
-      bar.style.borderLeftWidth = (dx - 2) + 'px';
+      bar.style.borderLeftWidth = (barWidth - gapBetweenBars) + 'px';
       sortingBox.appendChild(bar);
     }
-
-    console.log(arr);
   });
 
   document.querySelector(".header__sort").addEventListener("click", () => {
